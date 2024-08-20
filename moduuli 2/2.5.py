d9fd96d36c24df19b69b9797e8b03dc3e24cc904
koodihirmu <tomvar98@gmail.com>
2024-08-20 13:58:32 +0300

    refactoring

--- moduuli 2/2.5.py
+++ moduuli 2/2.5.py
@@ -4,15 +4,19 @@
 
 def main():
     sum = 0
+    # ota kaikki inputit ja käännä ne suoraan grammoiksi
     u_input = float(input("Anna leiviskät.\n"))
     sum += u_input * 20 * 32 * 13.3
+
     u_input = float(input("Anna naulat.\n"))
     sum += u_input * 32 * 13.3
+
     u_input = float(input("Anna luodit.\n"))
     sum += u_input * 13.3
 
     print(f"Massa nykymittojen mukaan:")
-    print(f"{int(sum / 1000)} kilogrammaa ja {round(sum % 1000, 2)} grammaa")
+    print(f"{int(sum / 1000)} kilogrammaa ja \
+          {round(sum % 1000, 2)} grammaa")
 
     
 if __name__ == "__main__":
