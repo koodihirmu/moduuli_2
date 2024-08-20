1019bd66984b980a5fc27d687d8254be3d53f155
koodihirmu <tomvar98@gmail.com>
2024-08-20 13:09:09 +0300

    suorakulmio ohjelma tehty

--- 03-keskiarvo.py
+++
@@ -1,9 +0,0 @@
-import math
-
-def main():
-    radius = int(input("Anna radius: "))
-    pintaala = math.pi * radius * radius
-    print(f"Ympyrän pinta-ala: {pintaala}")
-
-if __name__ == "__main__":
-    main()
