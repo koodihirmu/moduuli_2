2281de52493e110ef039299ee2b6ad06c527ded1
koodihirmu <tomvar98@gmail.com>
2024-08-28 14:34:36 +0300

    pi off by one fix

--- moduuli 4/4.6-pi.py
+++ moduuli 4/4.6-pi.py
@@ -5,7 +5,7 @@ def main():
     pisteita = int(input("Anna pisteiden määrä: "))
     pisteita_ympyrassa = 0
 
-    for i in range(1, pisteita):
+    for i in range(0, pisteita):
         x = rd.uniform(-1, 1)
         y = rd.uniform(-1, 1)
         if (x**2+y**2 < 1):
---
+++ moduuli 6/6.6-pizza.py
@@ -0,0 +1,6 @@
+def main():
+    for i in range (1,5):
+        print("test")
+
+if __name__ == "__main__":
+    main()
