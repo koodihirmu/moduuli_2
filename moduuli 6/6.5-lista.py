5c5a28b7c1c3e0119ba0d1cb810541bc95013ce4
koodihirmu <tomvar98@gmail.com>
2024-08-28 14:27:36 +0300

    6.5

--- moduuli 6/6.5-lista.py
+++ moduuli 6/6.5-lista.py
@@ -1,14 +1,23 @@
 def karsi_parittomat(list: list) -> list:
     list_temp = []
     for i in list:
-        if i % 2 != 0:
+        if i % 2 == 0:
             list_temp.append(i)
     return list_temp
 
 
 def main():
-    list = [2,6,3,3,6,45,9,6]
 
+    list = [2,6,3,3,6,45,9,6]
+    list2 = karsi_parittomat(list)
+    
+    for i in list:
+        print(f"{i} ", end="")
+    print()
+
+    for i in list2:
+        print(f"{i} ", end="")
+    print()
 
 if __name__ == "__main__":
     main()
