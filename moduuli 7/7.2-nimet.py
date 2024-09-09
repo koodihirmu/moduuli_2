aecd92d0ffbf0e2674c6e2d8c6f4000f27c38f67
koodihirmu <tomvar98@gmail.com>
2024-09-09 10:15:32 +0300

    7.2 tehty

--- moduuli 7/7.2-nimet.py
+++ moduuli 7/7.2-nimet.py
@@ -1,4 +1,25 @@
+def kysy_nimi() -> str:
+    nimi = input("Anna nimi: ")
+    return nimi
+
+
 def main():
+    # tee setti "nimet" jonne varastoidaan syöte
+    nimet = set()
+
+    while True:
+        uusi_nimi = kysy_nimi()
+        if (uusi_nimi == ""):
+            break
+
+        if uusi_nimi in nimet:
+            print("Aiemmin syötetty nimi")
+        else:
+            print("Uusi nimi")
+            nimet.add(uusi_nimi)
+
+    for nimi in nimet:
+        print(nimi)
 
 
 if __name__ == "__main__":
