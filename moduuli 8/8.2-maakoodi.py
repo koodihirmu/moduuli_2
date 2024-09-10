acb8a6f96794a5b47a24d4764deb6cb97e723164
koodihirmu <tomvar98@gmail.com>
2024-09-10 12:55:12 +0300

    8.2 parempi

--- moduuli 8/8.2-maakoodi.py
+++ moduuli 8/8.2-maakoodi.py
@@ -10,10 +10,14 @@ connection = mysql.connect(
     autocommit= True
 )
 
-airport_types = {"large_airport": 0, "medium_airport": 0, "small_airport": 0,  "heliport": 0, "seaplane_base": 0,"balloonport": 0,"closed": 0,}
-
 def get_airport_count_by_iso(iso_country):
-    sql = f"select type from airport where iso_country = \"{iso_country}\""
+    # haku joka palauttaa tyypin ja määrän jokaiselle tyypille
+    sql = f'''select type, count(*)
+            from airport 
+            where iso_country = \"{iso_country}\"
+            group by type
+            '''
+    # suorita haku
     with connection.cursor(dictionary=True) as cursor:
         cursor.execute(sql)
         return cursor.fetchall()
@@ -25,14 +29,12 @@ def main():
         if u_input == "":
             break
         airport_data = get_airport_count_by_iso(u_input)
+
+        # printtaa tarvittava tieto
         if len(airport_data) > 0:
-            for airport in airport_data:
-                airport_types[airport["type"]] += 1
-                # print(airport)
-            
             print("Maakoodilla löytyy: ")
-            for key in airport_types.keys():
-                print(f"{key} - {airport_types[key]}")
+            for data in airport_data:
+                print(f"{data['type']} - {data['count(*)']}")
 
 
 if __name__ == "__main__":
